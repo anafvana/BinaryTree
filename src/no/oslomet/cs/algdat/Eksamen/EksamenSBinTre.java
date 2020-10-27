21e30670bdfd4f07760cb4133ccb0767ef0c8f7a
Ana <anafvana@gmail.com>
2020-10-27 23:31:58 +0100

    Oppgave 6 - fjernAlle() endelig fikset

--- src/no/oslomet/cs/algdat/Eksamen/EksamenSBinTre.java
+++ src/no/oslomet/cs/algdat/Eksamen/EksamenSBinTre.java
@@ -117,107 +117,6 @@ public class EksamenSBinTre<T> {
     }
 
     //Inspirert av Programkode 5.2.8 d)
-    public boolean fjern(T verdi) {
-        //handle null verdi
-        if (verdi == null || rot.verdi == null) return false;
-
-        //hjelpevariabler
-        Node<T> p = førstePostorden(rot), pParent = p.forelder;
-
-        //søker etter verdien
-        if (p.verdi != null) {
-            while (p != rot) {
-                int temp = comp.compare(verdi, p.verdi);
-                if (temp != 0) {
-                    p = nestePostorden(p);
-                    pParent = p.forelder;
-                }
-                //finner verdien
-                else break;
-            }
-        }
-
-        if (p == rot) {
-            int temp = comp.compare(verdi, p.verdi);
-            if (temp != 0) return false;
-        }
-
-        //hvis noden som fjernes har mindre enn to barn
-        if (p.venstre == null || p.høyre == null){
-            Node<T> barn;
-            if (p.venstre != null) barn = p.venstre;
-            else barn = p.høyre;
-
-            //hvis noden har ingen barn
-            if (barn == null && p != rot){
-                if (p == pParent.venstre) {
-                    pParent.venstre = null;
-                } else if (p == pParent.høyre){
-                    pParent.høyre = null;
-                }
-            }
-            //hvis noden har et barn
-            else {
-                if (p == rot) {
-                    rot = barn;
-                } else if (p == pParent.venstre) {
-                    pParent.venstre = barn;
-                } else {
-                    pParent.høyre = barn;
-                }
-                if (barn != null) barn.forelder = pParent;
-            }
-            //fjerner alle pekere fra p
-            p.forelder = null;
-            p.høyre = null;
-            p.venstre = null;
-        }
-        //hvis noden som fjernes har begge barn
-        else {
-            //bruker inorder til å remplasere fjernet noden
-            Node<T> rParent = p, r = p.høyre;
-            while (r.venstre != null){
-                rParent=r;
-                r=r.venstre;
-            }
-
-            p.verdi = r.verdi;
-
-            if (rParent != p) rParent.venstre = r.høyre;
-            else rParent.høyre = r.høyre;
-
-            //etablerer ny forelder relasjon til r's (former) barn som blir rParents sin barn
-            rParent.høyre.forelder = rParent;
-
-            //fjerner alle pekere fra r
-            r.forelder = null;
-            r.høyre = null;
-            r.venstre = null;
-        }
-        antall--;
-        return true;
-    }
-
-    public int fjernAlle(T verdi) {
-        int fjernet = 0;
-        Node<T> p = førstePostorden(rot), q=null;
-
-        while (p != rot){
-            q=nestePostorden(p);
-            if(p.verdi == verdi){
-                boolean ok = fjernNode(p, verdi);
-                if (ok) fjernet++;
-                p=q;
-            }
-        }
-        if (rot.verdi == verdi){
-            boolean ok = fjernNode(rot, verdi);
-            if (ok) fjernet++;
-        }
-
-        return fjernet;
-    }
-
     private boolean fjernNode(Node<T> p, T verdi){
         if (p == rot) {
             int temp = comp.compare(verdi, p.verdi);
@@ -280,6 +179,49 @@ public class EksamenSBinTre<T> {
         return true;
     }
 
+    //Inspirert av Programkode 5.2.8 d)
+    public boolean fjern(T verdi) {
+        //handle null verdi
+        if (verdi == null || rot.verdi == null) return false;
+
+        //hjelpevariabler
+        Node<T> p = førstePostorden(rot), pParent = p.forelder;
+
+        //søker etter verdien
+        if (p.verdi != null) {
+            while (p != rot) {
+                int temp = comp.compare(verdi, p.verdi);
+                if (temp != 0) {
+                    p = nestePostorden(p);
+                    pParent = p.forelder;
+                }
+                //finner verdien
+                else break;
+            }
+        }
+
+        return fjernNode(p, verdi);
+    }
+
+    public int fjernAlle(T verdi) {
+        int fjernet = 0;
+        Node<T> p = førstePostorden(rot), q;
+        while (p != rot){
+            q=nestePostorden(p);
+            if(p.verdi == verdi){
+                boolean ok = fjernNode(p, verdi);
+                if (ok) fjernet++;
+            }
+            p=q;
+        }
+        if (rot.verdi == verdi){
+            boolean ok = fjernNode(rot, verdi);
+            if (ok) fjernet++;
+        }
+
+        return fjernet;
+    }
+
     public int antall(T verdi) {
         int result = 0;
         //Sjekk om verdien er null
@@ -316,7 +258,10 @@ public class EksamenSBinTre<T> {
         p.høyre = null;
         p.venstre = null;
         p.forelder = null;
-        antall--;
+        if (p!=rot || p.verdi != null){
+            p.verdi = null;
+            antall--;
+        }
     }
 
     private static <T> Node<T> førstePostorden(Node<T> p) {
