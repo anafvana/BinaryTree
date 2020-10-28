9f398acf6baebb5f09d9f165d0c10c18f26f750d
Ana <anafvana@gmail.com>
2020-10-28 17:58:52 +0100

    Oppgave 6 - Lagt til en siste test i main.

--- src/no/oslomet/cs/algdat/Eksamen/Main.java
+++ src/no/oslomet/cs/algdat/Eksamen/Main.java
@@ -19,5 +19,16 @@ public class Main {
         System.out.println(tre.antall(4));
         System.out.println(tre.antall(7));
         System.out.println(tre.antall(10));
+
+        //OPPGAVE 6 Test
+        a = new Integer[]{4, 7, 2, 9, 4, 10, 8, 7, 4, 6, 1};
+        tre = new EksamenSBinTre<>(Comparator.naturalOrder());
+        for(int verdi : a) tre.leggInn(verdi);
+        System.out.println(tre.fjernAlle(4)); //3
+        tre.fjernAlle(7); tre.fjern(8);
+        System.out.println(tre.antall()); //5
+
+        System.out.println(tre + " " + tre.toStringPostOrder());
+        // [1, 2, 6, 9, 10] [10, 9, 6, 2, 1]
     }
 }
